a9cd6cc97699a2783323f7bb1a36eeaec9f43409
vicotr-gold <victor.popanode93@gmail.com>
2022-07-25 16:56:21 +0800

    added logging

--- middlewares/check_email.js
+++ middlewares/check_email.js
@@ -4,6 +4,16 @@ var databases = require("../databases");
 
 function emailCheck(req, res, next) {
   if (validator.validate(req.headers["x-user"])) {
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    const requestUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+    const requestMethod = req.method;
+    databases.logging.insert({
+      user: req.headers["x-user"],
+      user_ip: ip,
+      timestamp: new Date(),
+      request_url: requestUrl,
+      request_method: requestMethod,
+    });
     next();
   } else {
     res.status(400).send({ statusCode: 400, error: "Bad Request", message: "Missing x-user in request header." });
